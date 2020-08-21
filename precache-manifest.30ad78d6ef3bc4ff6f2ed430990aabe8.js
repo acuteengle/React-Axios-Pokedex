ddd4ed5163e60aa7a2f415ac9d2844681b446b75
Brandon Eng <bce25@cornell.edu>
2020-08-21 12:37:03 -0700

    Updates

---
+++ precache-manifest.30ad78d6ef3bc4ff6f2ed430990aabe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f485abb07cb86ac2006a736fddd2552c",
+    "url": "/ReactAxiosPokedex/index.html"
+  },
+  {
+    "revision": "01ececc67607b974b1b1",
+    "url": "/ReactAxiosPokedex/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "36109d52cafabc41871a",
+    "url": "/ReactAxiosPokedex/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "01ececc67607b974b1b1",
+    "url": "/ReactAxiosPokedex/static/js/2.6e94768e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactAxiosPokedex/static/js/2.6e94768e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36109d52cafabc41871a",
+    "url": "/ReactAxiosPokedex/static/js/main.8a7b7466.chunk.js"
+  },
+  {
+    "revision": "540825eb9c7fee1f57d4",
+    "url": "/ReactAxiosPokedex/static/js/runtime-main.6dcfe6dc.js"
+  }
+]);
